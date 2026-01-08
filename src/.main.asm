cdd7a72feff62e768c341bb86ecaa0e282886e67
root <root@localhost.localdomain>
2026-01-08 13:00:10 +0000

    Rewrite assembly to use boolean logic

--- src/.main.asm
+++ src/.main.asm
@@ -3,18 +3,18 @@
 
 
 ;.include "../../setup/m328Pdef/m328Pdef.inc"
-.include "/home/subho/m328Pdef.inc"
+.include "/root/m328Pdef.inc"
 
 sbi DDRB, 5 ;set pin 13 as output pin (DDRB pin 5)
 ldi r16, 0b00000101 ;the last 3 bits define the prescaler, 101 => division by 1024
 out TCCR0B, r16 
 
-;  ldi r16,0 		;reset system status
-;  out SREG,r16		;init stack pointer
-;   ldi r16,low(RAMEND)	;0xff
-;   out SPL,r16
-;   ldi r16,high(RAMEND)	;0x08
-;   out SPH,r16
+  ldi r16,0 		;reset system status
+  out SREG,r16		;init stack pointer
+   ldi r16,low(RAMEND)	;0xff
+   out SPL,r16
+   ldi r16,high(RAMEND)	;0x08
+   out SPH,r16
 
   ldi r16,0b00111100	;identifying output pins 2,3,4,5
   out DDRD,r16		;declaring pins as output
@@ -22,34 +22,19 @@ out TCCR0B, r16
   ldi xh,0x01		;loading memory address higher byte into r27
   ldi r16,0b00000000	;initializing W 
   st  x,r16		;storing W in 0x0100 address
-  ldi r17,0x09		;initializing  count
-
-  ;loading numbers 0-9 into memory locations 0x0100-0x0109
-
-loop_cnt:
-  
-  inc r16		;increment register value
-  inc xl		;increment address
-  st  x, r16		;store number into memory
-  dec r17		;decrement count
-  brne loop_cnt 
 
 ;start printing numbers from 0-9
 
 Start:
 
-  ldi r17, 0x0A		;load the number 10 in r17
-  clr xl		;reset memory to 0x0100
-
-loop_inc:
-;writing W to pin 2
-  ldi r16, 0b00000010	;counter = 0
+  ldi r22, 0b00000010	;counter = 0
   ld  r0, x		;load number from memory
+  mov r16, r0
 
 loopw:
 
   lsl r0		;left shift
-  dec r16		;counter --
+  dec r22		;counter --
   brne loopw		;if counter !=0
 
   out PORTD,r0		;writing output to pins 2,3,4,5
@@ -60,36 +45,27 @@ loopw:
   ldi r19, 11
   call PAUSE
 
-  inc xl		;incrementing address
-  dec r17		;decrement decade count
-  brne loop_inc 	;branch if decade count !=0
-  
-  rjmp Start
+      ; We want r16 = r16 + 1 using only logic gates
+    ldi r17, 1              ; We are adding '1' to the accumulator
 
-;;delay routine
-;wait:
-;  push r16		;save register contents
-;  push r17		
-;  push r18		
-;
-;  ldi r16, 0x20		;loop 0x400000 times
-;  ldi r17, 0x00		;12 million cycles
-;  ldi r18, 0x00		;0.7s at 16 MHz
-;
-;w0:
-;  
-;  dec r18
-;  brne w0
-;  dec r17
-;  brne w0
-;  dec r16
-;  brne w0
-;
-;  pop r18		;restore register contents
-;  pop r17	
-;  pop r16
-;
-;  ret
+add:
+    mov r18, r16            ; Save copy of current accumulator
+    eor r16, r17            ; SUM = A XOR B (Calculate sum without carry)
+    and r18, r17            ; CARRY = A AND B (Calculate where carries happen)
+    lsl r18                 ; Shift Carry to the left (Carry goes to next bit)
+    mov r17, r18            ; Move new carry to r17
+    tst r17                 ; Check if Carry is 0
+    brne add        ; If Carry exists, repeat loop
+
+
+    cpi r16, 10             ; Compare result with 10
+    brne save       ; If not 10, skip reset
+    ldi r16, 0              ; If 10, reset counter to 0
+
+save:
+    st x, r16               ; Save new value to memory
+    rjmp Start
+
 
 PAUSE:	;this is delay (function)
 lp2:	;loop runs 64 times
