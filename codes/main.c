df9a181bbb8d990231e6bbb68767719d87c52bb1
root <root@localhost.localdomain>
2026-01-08 13:01:17 +0000

    Rewrite embC to use boolean logic. Draft 1

--- codes/main.c
+++ codes/main.c
@@ -8,17 +8,14 @@ extern void load_mem(void);
 //delayasm.S
 extern void delay(uint8_t);
 //dispscrnasm.S
-extern void disp_scrn(uint8_t);
+extern void disp_scrn(void);
 
 int main (void) {
 	init();
 	load_mem();
 	while (1) {
-		short t = 10;
-		while (t--) {
-			disp_scrn(9-t);
-			delay(61);
-		}
+		disp_scrn();
+		delay(61);
 	}
 	return 0;
 }
